0860bc9528fb2b47b4762eb1d703142665056b1c
novedadestec <70495680+novedadestec@users.noreply.github.com>
2020-09-03 00:41:21 -0500

    Delete boton.js

--- js/boton.js
+++
@@ -1,7 +0,0 @@
-function accion(){
-    console.log('está funcionando mi boton');
-    var ancla = document.getElementsByClassName('nav-enlace');
-    for(var i = 0; i < ancla.length; i++){
-        ancla[i].classList.toggle('desaparece');
-    }
-}
